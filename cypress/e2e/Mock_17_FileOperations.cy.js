269da7d851fd92dbf83c17933d3a7e4b2a57c66f
Swaroop D <swaroopmdongare@gmail.com>
2025-07-26 22:46:24 +0530

    mock updates

---
+++ cypress/e2e/Mock_17_FileOperations.cy.js
@@ -0,0 +1,8 @@
+//read and write file
+//cy.writeFile('path/to/message.txt', 'Hello World')
+// cy.readFile('path/to/message.txt').then((text) => {
+//     expect(text).to.equal('Hello World') // true
+//   })
+
+//upload file
+// cy.selectFile('path/to/file.txt')
